aab3a312af804cbf52ddb3526063df7efd301e62
Pancheri Renato (ACC0083848) <renato.pancheri@sci-b106pc051x.unitn.it>
2016-11-09 12:51:28 +0100

    inserimento della funzione nascondi/visualizza per il pulsante order items

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
-alert("ciao");
+var display=false;
+
+function showhide(){
+	if(display){
+		document.getElementById("item").className="nascosto";
+		document.getElementById("quantity").className="nascosto";
+		document.getElementById("update").className="nascosto";
+
+	}
+	else{
+		document.getElementById("item").className="";
+		document.getElementById("quantity").className="";
+		document.getElementById("update").className="";
+	}
+	display=!display;
+
+}
