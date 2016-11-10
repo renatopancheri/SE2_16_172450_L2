1e9154afe2f025dd09904bc457d5eb05396c7db6
Pancheri Renato (ACC0083848) <renato.pancheri@sci-b106pc051x.unitn.it>
2016-11-10 09:10:43 +0100

    aggiunta funzioen additem()

--- js.js
+++ js.js
@@ -1,5 +1,10 @@
+var items=[];
+var quantity=[];
 var display=false;
+/*
+	visualizza / nasconde le label per inserire item , quantity e il pulsante add
 
+*/
 function showhide(){
 	if(display){
 		document.getElementById("item").className="nascosto";
@@ -13,5 +18,30 @@ function showhide(){
 		document.getElementById("update").className="";
 	}
 	display=!display;
+}
+/*
+	aggiorna la quantità di un item o se l'item non è presente, lo aggiunge
 
+
+
+*/
+function additem(){
+	var found=false;
+	var item=document.getElementById("item").value;
+	var q=parseInt(document.getElementById("quantity").value);
+	if(!isNaN(q)){//controllo che la quantità sia un numero
+		for(var i=0;i<items.length && !found;i++){
+			if(items[i]==item){//ho trovato l'item nella tabella, aggiorno la quantità
+				found=true;
+				quantity[i]+=parseInt(q);
+				document.getElementById("tabella").rows[1].cells[i].innerHTML=quantity[i];
+			}
+		}
+		if(!found){// non ho trovato l'item nella tabella, lo aggiungo
+			items.push(item);
+			quantity.push(q);
+			document.getElementById("tabella").rows[0].insertCell(-1).innerHTML=item;
+			document.getElementById("tabella").rows[1].insertCell(-1).innerHTML=q;
+		}
+	}
 }
